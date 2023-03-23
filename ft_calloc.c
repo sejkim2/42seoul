1f19067473fe8aad7ce7f8903e023435b4049506
Sejin Kim <sejkim2@c2r10s7.42seoul.kr>
2023-03-23 14:23:14 +0900

    libft

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,32 +6,22 @@
 /*   By: sejkim2 <sejkim2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 17:56:27 by sejkim2           #+#    #+#             */
-/*   Updated: 2023/03/21 18:22:20 by sejkim2          ###   ########.fr       */
+/*   Updated: 2023/03/23 13:56:41 by sejkim2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+void    *ft_calloc(size_t count, size_t size)
 {
-	unsigned char	*p;
-	size_t			i;
+    unsigned char   *p;
+    size_t          i;
 
-	i = 0;
-	if (size == 0 || count == 0)
-	{
-		p = (unsigned char *)malloc(1);
-		if (p == 0)
-			return (0);
-		p[i] = 0;
-	}
-	else
-	{
-		p = (unsigned char *)malloc(size * count);
-		if (p == 0)
-			return (0);
-		while (i < count * size)
-			p[i++] = 0;
-	}
-	return (p);
+    i = 0;
+    p = (unsigned char *)malloc(count * size);
+    if (p == 0)
+        return (0);
+    while (i < count * size)
+        p[i++] = 0;
+    return (p);
 }
--- ft_split.c
+++ ft_split.c
@@ -6,51 +6,48 @@
 /*   By: sejkim2 <sejkim2@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 17:57:36 by sejkim2           #+#    #+#             */
-/*   Updated: 2023/03/22 18:04:50 by sejkim2          ###   ########.fr       */
+/*   Updated: 2023/03/23 13:57:38 by sejkim2          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 size_t	row_size(char const *s, char c)
 {
 	size_t	i;
 	size_t	count;
-	size_t	len;
 
-	len = ft_strlen(s);
 	i = 0;
 	count = 0;
-	while (s[i] == c || s[i] == '\0')
-		i++;
-	while (i <= len)
+	while (s[i])
 	{
-		if (s[i] == c || s[i] == '\0')
+		while (s[i] == c)
+			i++;
+		if (s[i])
 		{
-			while (s[i] == c || s[i] == '\0')
+			while (s[i] != c && s[i])
 				i++;
 			count++;
 		}
-		else
-			i++;
 	}
 	return (count);
 }
 
-void	make_split_string(char **table, const char *s, size_t head, size_t tail)
+int	make_split_string(char **table, const char *s, size_t head, size_t tail)
 {
 	size_t	i;
 
 	i = 0;
 	*table = (char *)malloc(sizeof(char) * (tail - head + 1));
+	if (*table == 0)
+		return (0);
 	while (i < tail - head)
 	{
 		(*table)[i] = s[i + head];
 		i++;
 	}
 	(*table)[i] = '\0';
-	printf("%s\n", *(table));
+	return (1);
 }
 
 char	**ft_split(char const *s, char c)
@@ -58,39 +55,34 @@ char	**ft_split(char const *s, char c)
 	char	**table;
 	size_t	row;
 	size_t	i;
-	size_t	s_len;
 	size_t	head;
+	size_t	j;
 
+	j = 0;
 	i = 0;
 	row = 0;
-	s_len = ft_strlen(s);
 	table = (char **)malloc(sizeof(char *) * (row_size(s, c) + 1));
 	if (table == 0)
 		return (0);
-	while (s[i] && s[i] == c)
-		i++;
-	head = i;
-	while (i <= s_len)
-	{
-		if (s[i] == '\0' || s[i] == c)
-		{
-			make_split_string(&(table[row]), s, head, i);
-			while (s[i] == '\0' || s[i] == c)
-				i++;
-			row++;
-			head = i;
-		}
-		else
-			i++;
-	}
-	if (row > 1)
-		table[row] = 0;
+	while (s[i])
+    {
+        while (s[i] == c)
+            i++;
+        if (s[i])
+        {
+            head = i;
+            while (s[i] != c && s[i])
+                i++;
+            if (!make_split_string(&(table[row]), s, head, i))
+			{
+                while (j < row)
+                    free(table[j++]);
+                free(table);
+                return (0);
+            }
+            row++;
+        }
+    }
+	table[row] = 0;
 	return (table);
-}
-
-int main()
-{
-    char **str = ft_split(",,,,,,,", ',');
-    for(size_t i = 0; i < 5; i++)
-        printf("%zu:%s\n", i, str[i]);
 }
