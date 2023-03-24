3548588b1d07d17b70b99d2e0aa23ab2f6fe3a63
sejkim2 <128696540+sejkim2@users.noreply.github.com>
2023-03-24 16:48:31 +0900

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -12,6 +12,7 @@
 
 #include "libft.h"
 
+//n바이트 만큼 문자열끼리 비교
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	unsigned char	*p1;
@@ -21,7 +22,7 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	p1 = (unsigned char *)s1;
 	p2 = (unsigned char *)s2;
 	i = 0;
-	if (p1 == 0 && p2 == 0)
+	if (p1 == p2)	//둘 다 0포인터 이거나 (저장된 것이 없음, 초기 주소) 주소가 같으면 같은 문자열이므로 0 반환
 		return (0);
 	while (i < n)
 	{
