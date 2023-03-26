de417b74a32859f7d7238b09a01b35a7a8846f99
sejkim2 <128696540+sejkim2@users.noreply.github.com>
2023-03-26 13:16:19 +0900

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -12,15 +12,17 @@
 
 #include "libft.h"
 
+//문자열을 그대로 복사하여 생성된 새 문자열 반환
 char	*ft_strdup(const char *s1)
 {
 	char	*dest;
 	int		i;
 
-	dest = (char *)malloc((ft_strlen(s1) + 1) * sizeof(char));
+	dest = (char *)malloc((ft_strlen(s1) + 1) * sizeof(char));	//널을 고려하여 +1
 	if (dest == 0)
 		return (0);
 	i = 0;
+	//strlcpy(dest, s1, ft_strlen(s1) + 1))과 
 	while (s1[i])
 	{
 		dest[i] = s1[i];
