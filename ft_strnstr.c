d50b2c8790fabcaafa435788b340e38d44fc412e
sejkim2 <128696540+sejkim2@users.noreply.github.com>
2023-03-26 14:23:59 +0900

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,6 +12,7 @@
 
 #include "libft.h"
 
+//길이 내에서 부분 문자열이 있는지 조사하여 있다면 그 주소 반환
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
@@ -23,17 +24,18 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	str = (char *)haystack;
 	to_find = (char *)needle;
 	needle_size = ft_strlen(needle);
-	if (*to_find == 0)
+	if (*to_find == 0)		//찾고자 하는 문자열이 (널로) 비어있을 때 (널포인터와는 다름)
 		return (str);
 	i = 0;
 	while (str[i] && i < len)
 	{
-		if (str[i] == to_find[0])
+		if (str[i] == to_find[0])	//첫번째 문자가 일치하면 조사할 조건을 만족
 		{
-			j = 0;
+			j = 0;	//j = 1이 더 정확
+			//길이 내에서 && 찾고자 하는 문자열의 길이 내에서 && 같은지 조사하면서
 			while (i + j < len && j < needle_size && str[i + j] == to_find[j])
 				j++;
-			if (j == needle_size)
+			if (j == needle_size)	//반복문 탈출 이후 크기를 조사해서 그 크기가 찾고자 하는 문자열 크기와 같다면 전부 조사해서 문자가 
 				return (str + i);
 		}
 		i++;
