470ec6783750ab40655b7777d018a1d2aee13b27
Sejin Kim <kimsejin2118@gmail.com>
2023-04-18 17:52:08 +0900

    gnl

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sejkim2 <sejkim2@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/14 13:36:39 by sejkim2           #+#    #+#             */
+/*   Updated: 2023/04/18 12:12:08 by sejkim2          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_nd
+{
+	int				fd;
+	char			*buf;
+	struct s_nd		*next;
+	struct s_nd		*prev;
+}	t_nd;
+
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*get_next_line(int fd);
+char	*split_two_str(char **buf);
+char	*ft_strchr(const char *s, int c);
+
+#endif
