297b1d2fc64d81f85fc85f1cd2bc487e2152924b
Sejin Kim <sejkim2@c2r9s5.42seoul.kr>
2023-08-03 16:56:06 +0900

    0803

--- circle_2/pipex/srcs/run_pipex.c
+++ circle_2/pipex/srcs/run_pipex.c
@@ -1,35 +1,34 @@
 #include "../includes/pipex.h"
 
+static void child_process(t_node *node, char **envp)
+{
+    close(node->pipe_fd[0]);
+    dup2(node->pipe_fd[1], STDOUT_FILENO);
+    dup2(node->infile_fd, STDIN_FILENO);
+    close(node->pipe_fd[1]);
+    close(node->infile_fd);
+    execve(node->path_env1, node->cmd1, envp);
+}
+
+static void parent_process(t_node *node, char **envp)
+{
+    close(node->pipe_fd[1]);
+    dup2(node->pipe_fd[0], STDIN_FILENO);
+    dup2(node->outfile_fd, STDOUT_FILENO);
+    close(node->pipe_fd[0]);
+    close(node->outfile_fd);
+    waitpid(node->pid, 0, 0);
+    execve(node->path_env2, node->cmd2, envp);
+}
+
 void run_pipex(t_node *node, char **envp)
 {
-    if (pipe(node->pipe_fd) < 0)
-    {
-        ft_printf("pipe error\n");
-        exit(1);
-    }
+    pipe(node->pipe_fd);
     node->pid = fork();
     if (node->pid == -1)
-    {
-        ft_printf("fork error\n");
         exit(1);
-    }
     else if (node->pid == 0)
-    {
-        close(node->pipe_fd[0]);
-        dup2(node->pipe_fd[1], STDOUT_FILENO);
-        dup2(node->infile_fd, STDIN_FILENO);
-        close(node->pipe_fd[1]);
-        close(node->infile_fd);
-        execve(node->path_env1, node->cmd1, envp);
-    }
+        child_process(node, envp);
     else
-    {
-        close(node->pipe_fd[1]);
-        dup2(node->pipe_fd[0], STDIN_FILENO);
-        dup2(node->outfile_fd, STDOUT_FILENO);
-        close(node->pipe_fd[0]);
-        close(node->outfile_fd);
-        waitpid(node->pid, 0, 0);
-        execve(node->path_env2, node->cmd2, envp);
-    }
+        parent_process(node, envp);
 }
