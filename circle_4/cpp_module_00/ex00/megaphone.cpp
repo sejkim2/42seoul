a9735dfde907beff32ac039e044c7d3790175ada
Sejin Kim <sejkim2@c2r10s3.42seoul.kr>
2024-02-01 20:58:26 +0900

    cpp00

---
+++ circle_4/cpp_module_00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+static	void	print_char(char ch)
+{
+	if (ch >= 'a' && ch <= 'z')
+		ch = ch - 'a' + 'A';
+	std::cout << ch << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 0)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for(int i = 1; i<argc; i++)
+		{
+			int j = 0;
+			while (argv[i][j])
+			{
+				print_char(argv[i][j]);
+				j++;
+			}
+		}
+	}
+}
