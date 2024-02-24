4c91d021cc625693321c313e5f237050839118f5
Sejin Kim <sejkim2@c2r10s3.42seoul.kr>
2024-02-24 21:09:39 +0900

    fix : cpp01_ex06 출력문

--- circle_4/cpp_module_01/ex06/Harl.cpp
+++ circle_4/cpp_module_01/ex06/Harl.cpp
@@ -26,7 +26,8 @@ void Harl::info( void )
 void Harl::warning( void )
 {
     std::cout << "[ WARNING ]" << '\n';
-    std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << '\n';
+    std::cout << "I think I deserve to have some extra bacon for free." << '\n';
+    std::cout << "I've been coming for years whereas you started working here since last month." << '\n';
 }
 
 void Harl::error( void )
@@ -38,8 +39,6 @@ void Harl::error( void )
 void Harl::complain(std::string level)
 {
     int index = -1;
-    typedef void (Harl::*funcArr)();
-    funcArr arr[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 
     for(size_t i = 0; i < levels->length(); i++)
     {
@@ -49,26 +48,25 @@ void Harl::complain(std::string level)
             break ;
         }
     }
-    if (index == -1)
-    {
-        std::cout << "[ Probably complaining about insignificant problems ]" << '\n';
-        return;
-    }
+
     switch (index)
     {
+        case -1:
+            std::cout << "[ Probably complaining about insignificant problems ]" << '\n';
+            break ;
         case 0:
-            (*this.*arr[index])();
+            Harl::debug();
+            std::cout << '\n';
         case 1:
-            (*this.*arr[index])();
+            Harl::info();
+            std::cout << '\n';
         case 2:
-            (*this.*arr[index])();
+            Harl::warning();
+            std::cout << '\n';
         case 3:
-            (*this.*arr[index])();
+            Harl::error();
+            std::cout << '\n';
         default:
             break;
     }
 }
-
-
-
-s
