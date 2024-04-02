81f720b9147ff13cbbe0c00fda089a267ae19ed2
Sejin Kim <sejkim2@c2r10s1.42seoul.kr>
2024-04-02 16:49:13 +0900

    add : cpp04_ex03 files

---
+++ circle_4/cpp_module_04/ex03/AMateria.hpp
@@ -0,0 +1,23 @@
+#ifndef _AMATERIA
+# define _AMATERIA
+
+# include <iostream>
+# include <string>
+# include "ICharacter.hpp"
+
+class AMateria
+{
+    public:
+        /*canonical form*/
+        AMateria(void);
+        virtual ~AMateria(void);
+        AMateria(const AMateria& amateria);
+        AMateria& operator=(const AMateria& amateria);
+
+        AMateria(std::string const & type);
+        std::string const & getType() const;
+        virtual AMateria* clone() const = 0;
+        virtual void use(ICharacter& target);
+};
+
+#endif
