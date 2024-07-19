9e51a3e5ad3affb2b066af035a1f210aa6e654d0
kimsejin <kimsejin2118@gmail.com>
2024-07-19 20:22:20 +0900

    add : cpp06 nan, inff

--- circle_5/cpp_module_06/ex00/ScalarConverter.cpp
+++ circle_5/cpp_module_06/ex00/ScalarConverter.cpp
@@ -23,51 +23,68 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& obj)
     return (*this);
 }
 
+//nan : 부동소수점 체계에서 사용
+//지수부 비트가 모두 1, 가수부 비트는 최소 하나는 1
+//수학적으로 정의되지 않은 결과를 나타내기 위해 사용
+
+//inf : 부동소수점 체계에서 사용
+//지수부 비트는 모두 1, 가수부 비트는 모두 0
+//매우 큰 수를 나타내기 위해 사용
 void ScalarConverter::convert(std::string literal)
 {
     Literal checkLiteral;
     char *end;
 
+    std::string convertChar;
+    std::string convertInt;
+    std::string convertFloat;
+    std::string convertDouble;
     
-    // std::string convertChar;
-    // std::string convertInt;
-    // std::string convertFloat;
-    // std::string convertDouble;
-    
+    if (literal.compare("-inff") == 0)
+    {
+        convertChar = "impossible";
+        convertInt = "impossible";
+        convertFloat = "-inf";
+        convertDouble = "-inff";
+    }
+    else if (literal.compare("+inff") == 0)
+    {
+        convertChar = "impossible";
+        convertInt = "impossible";
+        convertFloat = "inf";
+        convertDouble = "inff";
+    }
+    else if (literal.compare("nanf") == 0)
+    {
+        convertChar = "impossible";
+        convertInt = "impossible";
+        convertFloat = "nanf";
+        convertDouble = "nan";
+    }
+    else if (literal.compare("-inf") == 0)
+    {
+        convertChar = "impossible";
+        convertInt = "impossible";
+        convertFloat = "-inf";
+        convertDouble = "-inff";
+    }
+    else if (literal.compare("+inf") == 0)
+    {
+        convertChar = "impossible";
+        convertInt = "impossible";
+        convertFloat = "inff";
+        convertDouble = "inf";
+    }
+    else if (literal.compare("nan") == 0)
+    {
+        convertChar = "impossible";
+        convertInt = "impossible";
+        convertFloat = "nanf";
+        convertDouble = "nan";
+    }
+    else
+    {
 
-    // if (literal.compare("-inff") == 0)
-    // {
-        
-    // }
-    // else if (literal.compare("+inff") == 0)
-    // {
-
-    // }
-    // else if (literal.compare("nanf") == 0)
-    // {
-
-    // }
-    // else if (literal.compare("-inf") == 0)
-    // {
-
-    // }
-    // else if (literal.compare("+inf") == 0)
-    // {
-    //     convertChar = "impossible";
-    //     convertInt = "+inf";
-    //     convertFloat = "+inff";
-    //     convertDouble = "nan";
-    // }
-    // else if (literal.compare("nan") == 0)
-    // {
-    //     convertChar = "impossible";
-    //     convertInt = "impossible";
-    //     convertFloat = "nanf";
-    //     convertDouble = "nan";
-    // }
-    // else
-    // {
-
-    // }
+    }
     
 }
---
+++ circle_5/cpp_module_06/ex00/test2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    double a = -1.0 / 0.0;
+    cout << a;
+}
