4f2dc318828370404d9b1aae17a7c3f4a51f7ed7
Sejin Kim <sejkim2@c4r1s6.42seoul.kr>
2024-09-17 15:28:08 +0900

    fix : cpp07ex00 const 인자

--- circle_5/cpp_module_07/ex00/func.hpp
+++ circle_5/cpp_module_07/ex00/func.hpp
@@ -12,13 +12,13 @@ void swap(T& a, T& b)
 }
 
 template <typename T>
-T min(const T& a, const T& b)
+T min(T& a, T& b)
 {
     return ((a < b) ? a : b);
 }
 
 template <typename T>
-T max(const T& a, const T& b)
+T max(T& a, T& b)
 {
     return ((a > b) ? a : b);
 }
