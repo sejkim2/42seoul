79b32f9c185ee59590a4f52b96d9bf38435eecc4
kimsejin <kimsejin2118@gmail.com>
2024-09-25 04:49:09 +0900

    add : cpp09ex02 main.cpp -> time check

--- circle_5/cpp_module_09/ex02/main.cpp
+++ circle_5/cpp_module_09/ex02/main.cpp
@@ -1,10 +1,11 @@
-# include "PmergeMe.hpp"
+#include "PmergeMe.hpp"
+#include <ctime>   
 
 bool allUnique(const std::vector<int>& numbers, std::set<int>& sortedNumbers) 
 {
     for (std::vector<int>::const_iterator it = numbers.begin(); it != numbers.end(); ++it) {
         if (sortedNumbers.find(*it) != sortedNumbers.end()) {
-            return true;
+            return false;
         }
         sortedNumbers.insert(*it);
     }
@@ -24,29 +25,23 @@ bool stringToPositiveInt(const std::string& str, int& number)
 
 void printNumbersBeforeSort(const std::vector<int>& numbers)
 {
-    std::cout << "Before:       ";
+    std::cout << "Before: ";
     for(std::vector<int>::const_iterator it = numbers.begin(); it != numbers.end(); it++)
         std::cout << *it << ' ';
     std::cout << '\n';
 }
 
-void printNumbersAfterSortVector(const std::vector<int>& numbers)
+template <typename T>
+void printNumbersAfterSort(const T& numbers)
 {
-    std::cout << "Vector After: ";
-    for(std::vector<int>::const_iterator it = numbers.begin(); it != numbers.end(); it++)
+    std::cout << "After: ";
+    for(typename T::const_iterator it = numbers.begin(); it != numbers.end(); it++)
         std::cout << *it << ' ';
     std::cout << '\n';
 }
 
-void printNumbersAfterSortList(const std::list<int>& numbers)
-{
-    std::cout << "List After:   ";
-    for(std::list<int>::const_iterator it = numbers.begin(); it != numbers.end(); it++)
-        std::cout << *it << ' ';
-    std::cout << '\n';
-}
-
-bool isSorted(const std::vector<int>& numbers, const std::set<int>& sortedNumbers)
+template <typename T>
+bool isSorted(const T& numbers, const std::set<int>& sortedNumbers)
 {
     if (numbers.size() != sortedNumbers.size())
         return (false);
@@ -67,7 +62,6 @@ int main(int argc, char* argv[])
     std::list<int> list;
     std::set<int> sortedNumbers;
     
-
     for (int i = 1; i < argc; ++i)
     {
         int number;
@@ -87,10 +81,24 @@ int main(int argc, char* argv[])
     {
         printNumbersBeforeSort(numbers);
         PmergeMe pm;
+        std::clock_t start;
+        std::clock_t end;
+        double elapsed_time;
+        
+        start = std::clock();
         std::vector<int> sorted_vector = pm.ford_johnson_by_vector(numbers);
+        end = std::clock();
+        elapsed_time = double(end - start) / CLOCKS_PER_SEC * 1e6;
+        std::cout << "Time to process a range of " << sorted_vector.size() << " elements with std::vector: " << elapsed_time << " us" << '\n';
+
+        start = std::clock();
         std::list<int> sorted_list = pm.ford_johnson_by_list(list);
-        printNumbersAfterSortVector(sorted_vector);
-        printNumbersAfterSortList(sorted_list);
+        end = std::clock();
+        elapsed_time = double(end - start) / CLOCKS_PER_SEC * 1e6;
+        std::cout << "Time to process a range of " << sorted_list.size() << " elements with std::list: " << elapsed_time << " us" << '\n';
+
+        printNumbersAfterSort(sorted_vector);
+        printNumbersAfterSort(sorted_list);
     }
 
     return 0;
